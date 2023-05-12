1d12313dbcaf498b9d9fd946fa5173ef0fcbe62c
nagrarohit <rohit.nagra7861@gmail.com>
2023-05-12 16:33:55 +0530

    constants for frontend added

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,30 +1,41 @@
 const networkConfig = {
+    default: {
+        name: "hardhat",
+        keepersUpdateInterval: "30",
+    },
     31337: {
         name: "localhost",
-        ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
-        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
-        mintFee: "10000000000000000", // 0.01 ETH
+        subscriptionId: "588",
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+        keepersUpdateInterval: "30",
+        raffleEntranceFee: "100000000000000000", // 0.1 ETH
         callbackGasLimit: "500000", // 500,000 gas
     },
-    // Price Feed Address, values can be obtained at https://docs.chain.link/data-feeds/price-feeds/addresses
     11155111: {
         name: "sepolia",
-        ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
-        vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
-        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+        subscriptionId: "588",
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
+        keepersUpdateInterval: "30",
+        raffleEntranceFee: "100000000000000000", // 0.1 ETH
         callbackGasLimit: "500000", // 500,000 gas
-        mintFee: "10000000000000000", // 0.01 ETH
-        subscriptionId: "1448", // add your ID here!
+    },
+    1: {
+        name: "mainnet",
+        keepersUpdateInterval: "30",
     },
 }
 
-const DECIMALS = "18"
-const INITIAL_PRICE = "200000000000000000000"
 const developmentChains = ["hardhat", "localhost"]
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6
+const frontEndContractsFile = "../nextjs-nft-marketplace-fcc/constants/networkMapping.json"
+
+const frontEndAbiLocation = "../nextjs-nft-marketplace-fcc/constants/"
 
 module.exports = {
     networkConfig,
     developmentChains,
-    DECIMALS,
-    INITIAL_PRICE,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+    frontEndContractsFile,
+
+    frontEndAbiLocation,
 }
